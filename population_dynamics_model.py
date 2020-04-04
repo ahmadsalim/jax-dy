527c815b7f2ddd5257606912abcb55dc50d3a2e9
Ahmad Salim Al-Sibahi <ahmad@di.ku.dk>
2020-04-04 18:38:13 +0200

    COVID-SEIR model

--- population_dynamics_model.py
+++ population_dynamics_model.py
@@ -2,36 +2,55 @@ import random
 import jax.numpy as np
 import jax.random as jaxran
 import jax.scipy.stats as sst
+import pandas as pd
 import numpyro
 from numpyro import handlers
 from numpyro.infer import MCMC, NUTS
 import numpyro.distributions as dist
 from runge_kutta import runge_kutta_4
 
-def model(obs, step_size=0.2):
-    y_0 = numpyro.sample('y_0', dist.HalfNormal(100))
-    k = 0.1 + numpyro.sample('k', dist.HalfNormal(100))
-    num_steps = int(obs.shape[0] / step_size)
-    sim = runge_kutta_4((lambda t, y: -k * y), y_0, step_size, num_steps)
-    sim = np.reshape(sim, (obs.shape[0], -1))[..., -1] + 0.1
-    with numpyro.plate('data', obs.shape[0], dim=-1):
-        numpyro.sample('obs', dist.Poisson(sim), obs=obs)
-
+def seir_model(initial_seir_state, num_days, infection_data, recovery_data, step_size=0.1):
+    if infection_data is not None:
+        assert num_days == infection_data.shape[0]
+    if recovery_data is not None:
+        assert num_days == recovery_data.shape[0]
+    beta = numpyro.sample('beta', dist.HalfCauchy(scale=1000.))
+    gamma = numpyro.sample('gamma', dist.HalfCauchy(scale=1000.))
+    sigma = numpyro.sample('sigma', dist.HalfCauchy(scale=1000.))
+    mu = numpyro.sample('mu', dist.HalfCauchy(scale=1000.))
+    nu = np.array(0.0) # No vaccine yet
+    def seir_update(day, seir_state):
+        s = seir_state[..., 0]
+        e = seir_state[..., 1]
+        i = seir_state[..., 2]
+        r = seir_state[..., 3]
+        n = s + e + i + r
+        s_upd = mu * (n - s) - beta * (s * i / n) - nu * s
+        e_upd = beta * (s * i / n) - (mu + sigma) * e
+        i_upd = sigma * e - (mu + gamma) * i
+        r_upd = gamma * i - mu * r + nu * s
+        return np.stack((s_upd, e_upd, i_upd, r_upd), axis=-1)
+    num_steps = int(num_days / step_size)
+    sim = runge_kutta_4(seir_update, initial_seir_state, step_size, num_steps)
+    sim = np.reshape(sim, (num_days, int(1/step_size), 4))[:, -1, :] + 1e-3
+    with numpyro.plate('data', num_days):
+        numpyro.sample('infections', dist.Poisson(sim[:, 2]), obs=infection_data)
+        numpyro.sample('recovery', dist.Poisson(sim[:, 3]), obs=recovery_data)
 
 if __name__ == "__main__":
-    true_k = np.array(0.5)
-    y_0 = np.array(300.)
-    num_days = 50
-    num_warmup = 1000
-    num_samples = 10000
-    x = np.linspace(1, num_days, num=num_days)
     rngkey = jaxran.PRNGKey(random.randint(0, 10000))
     rngkey, rngkey_ = jaxran.split(rngkey)
-    obs_rates = y_0 * np.exp(-true_k*x)
-    observations = np.maximum(dist.Poisson(obs_rates).sample(rngkey).transpose(), 0)
-    step_size = 1.0
-    num_steps = int(num_days / step_size)
-    kernel = NUTS(model)
+    num_days = 15
+    covid_data = pd.read_csv('data/COVID19_Denmark.csv')
+    population = covid_data['Population'][0]
+    initial_seir_state = np.array([population - 1., 1., 0., 0.])
+    infection_data = covid_data['StillInfected'].to_numpy()
+    recovery_data = covid_data['Recovered'].to_numpy() + covid_data['Deaths'].to_numpy() # Dead people are also recovered in the model
+    num_days = infection_data.shape[0]
+    step_size = 0.1
+    num_warmup = 2500
+    num_samples = 10000
+    kernel = NUTS(seir_model)
     mcmc = MCMC(kernel, num_warmup, num_samples)
-    mcmc.run(rngkey_, observations)
+    mcmc.run(rngkey_, initial_seir_state, num_days, infection_data, recovery_data, step_size=step_size)
     mcmc.print_summary()
--- runge_kutta.py
+++ runge_kutta.py
@@ -8,12 +8,12 @@ from jax.experimental import loops
 
 
 @functools.partial(jax.jit, static_argnums=(0,2,3))
-def runge_kutta_4(f: Callable[[np.ndarray, np.ndarray], np.ndarray], 
+def runge_kutta_4(f: Callable[[float, np.ndarray], np.ndarray], 
                   y0: np.ndarray,
                   step_size,
                   num_steps):
     with loops.Scope() as s:
-        s.res = np.empty(num_steps)
+        s.res = np.empty((num_steps, *y0.shape))
         s.y = y0
         for i in s.range(num_steps):
             t = i * step_size
@@ -24,8 +24,3 @@ def runge_kutta_4(f: Callable[[np.ndarray, np.ndarray], np.ndarray],
             s.y = s.y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
             s.res = ops.index_update(s.res, i, s.y)
         return s.res
-
-if __name__ == "__main__":
-    rkap = runge_kutta_4(lambda t, y: - 2 * y , np.array(3.), step_size=0.005)
-    ex = np.array([3*np.exp(-2*(i+1)*0.005) for i in range(1000)])
-    print(np.max(rkap - ex))
